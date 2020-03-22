df87263e41de5ef196558f22aa8740841989a44b
Trever Anderson <treveranderson01@gmail.com>
2020-03-22 14:17:18 -0500

    implemented cohesion

--- priors.py
+++ priors.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 from matplotlib import colors, pyplot
 from itertools import product
+import random
+from functools import reduce
 
 ARC_PATH = '/home/tanderson/git/ARC/'
 ARC_TRAIN = 'data/training'
@@ -16,14 +18,14 @@ YELLOW = 'y'
 GRAY = '0.75'
 PINK = '#FF69B4'
 ORANGE = '#FFA500'
-CYAN = 'c'
-MAROON = 'm'
+CYAN = '#00FFFF'
+MAROON = '#800000'
 
 COLOR_RANGE = range(10)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--input-task', required=True)
+    parser.add_argument('-ts', '--input-tasks', required=True, nargs='+')
     parser.add_argument('-a', '--arc-path', default=ARC_PATH)
 
     return parser.parse_args()
@@ -40,7 +42,7 @@ def display(grid):
                                   PINK, ORANGE, CYAN, MAROON])
     bounds = range(10)
     np_bs = np.array(bounds)
-    norm = colors.BoundaryNorm(bounds, cmap.N)
+    norm = colors.BoundaryNorm(bounds, cmap.N - 1)
     fig, ax = pyplot.subplots()
     ax.matshow(np_g, cmap=cmap, norm=norm)
     ax.grid(linewidth=2, color='0.5')
@@ -50,7 +52,7 @@ def display(grid):
     ax.set_yticklabels([])
     pyplot.show()
 
-def stoch_seed_sprinkle(grid, coverage=0.5):
+def stoch_seed_sprinkle(grid, coverage=1):
     r, c = np.indices(grid.shape)
     ps = np.asarray([(r, c) for r, c in zip(r.reshape(-1), c.reshape(-1))])
     lps = len(ps)
@@ -68,16 +70,69 @@ def grow(region, grid):
     result = set()
     for r, c in region:
         result = result.union(eight_conn((r, c), grid, lambda x: x == grid[r][c]))
-    return tuple(result)
+    return result
 
-def region_growing(grid, seeds):
-    regions = {((r, c),): grid[r][c] for r, c in seeds}
-    new_regions = {grow(r, grid): regions[r] for r in regions}
-    return new_regions
+def regions(grid, seeds):
+    regions = {}
+    for r, c in seeds:
+        pv = grid[r][c]
+        regions[pv] = regions[pv] + [{(r, c)}] if pv in regions else [{(r, c)}]
+    return regions
+
+def grow_regions(regions, grid):
+    return {pv: [grow(r, grid) for r in regs] for pv, regs in regions.items()}
+
+def merge(regions):
+    lr = len(regions)
+    new_regs = set()
+    for i in range(lr):
+        unions = []
+        for j in range(i+1, lr):
+            inter = regions[i].intersection(regions[j])
+            if inter != set():
+                unions.append(regions[i].union(regions[j]))
+        nr = reduce(lambda s1, s2: s1.union(s2), unions) if unions \
+             else regions[i]
+        if all([nr.intersection(set(t)) == set() for t in new_regs]):
+            new_regs.add(tuple(nr))
+
+    #ordering is arbitrary, but necessary for equality
+    return sorted([set(t) for t in new_regs], key=lambda s: list(s)[0][0])
+
+def merge_regions(all_regs):
+    return {pv: merge(regs) for pv, regs in all_regs.items()}
+
+def object_cohesion(in_grid):
+    grid = np.asarray(in_grid)
+    seeds = stoch_seed_sprinkle(grid)
+    regs = merge_regions(grow_regions(regions(grid, seeds), grid))
+    prev_regs = None
+    while prev_regs != regs:
+        prev_regs = regs
+        regs = merge_regions(grow_regions(regs, grid))
+    return regs
+
+def images_from_object_cohesion(oc, grid_shape):
+    for pv, regs in oc.items():
+        for rg in regs:
+            img = np.zeros(grid_shape)
+            for r, c in list(rg):
+                img[r, c] = pv
+            print("display for pv: {}, reg: {}".format(pv, rg))
+            display(img)
+
+def test_object_cohesion(tasks, task_path):
+    for t in tasks:
+        data = read_file(path.join(task_path, t))
+        tests = [data['train'][0]['input'], data['train'][0]['output']]
+        for t in tests:
+            oc = object_cohesion(t)
+            for pv, regs in sorted(oc.items(), key=lambda t: t[0]):
+                print("pixel_value: {}, num_regions: {}".format(pv, len(regs)))
+            print()
+            #images_from_object_cohesion(oc, np.asarray(t).shape)
 
 if __name__ == '__main__':
     args = parse_args()
-    task_path = path.join(*[ARC_PATH, ARC_TRAIN, args.input_task])
-    data = read_file(task_path)
-
-    display(data['train'][0]['input'])
+    task_path = path.join(*[ARC_PATH, ARC_TRAIN])
+    test_object_cohesion(args.input_tasks, task_path)
