de20f8f73f867ced3419c853cd59e1f845201174
Trever Anderson <treveranderson01@gmail.com>
2020-05-09 14:01:45 -0500

    removed old dsl file

--- dsl.py
+++
@@ -1,6 +0,0 @@
-import base_dsl as bdsl
-
-def gap_fill(gap_color, deviations):
-    return [bdsl.convert_value, lambda dat_map: {**dat_map, **{bdsl.VAL: deviations}},
-            bdsl.sparse_and_object_cohesion, bdsl.group_object_cohesion, 
-            bdsl.object_gaps, lambda dat_map: {**dat_map, **{bdsl.VAL2: gap_color}}, bdsl.color_gaps]
